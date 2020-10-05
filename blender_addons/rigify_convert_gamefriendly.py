312417e1e70edbd44d6ed546535ae4faa5ef55c5
pKrime <palucam@gmail.com>
2020-10-05 19:52:41 +0200

    rigify_game, added support for quadrupeds

--- blender_addons/rigify_convert_gamefriendly.py
+++ blender_addons/rigify_convert_gamefriendly.py
@@ -1,13 +1,14 @@
 import bpy
 from mathutils import Vector
 
+from bpy.props import BoolProperty
 
 bl_info = {
     "name": "Rigify - Make Game Friendly",
     "author": "Paolo Acampora",
     "version": (1, 0, 1),
     "blender": (2, 80, 0),
-    "location": "Armature properties",
+    "location": "Armature Context Menu (right click, armature edit mode)",
     "description": "Brings the DEF- bones of rigify 0.5 rigs into a single, game friendly hierarchy",
     "category": "Rigging",
     }
@@ -29,67 +30,74 @@ def copy_bone_constraints(bone_a, bone_b):
                 continue
 
 
-def copy_bone(obj, bone_name, assign_name='', constraints=False):
+def copy_bone(ob, bone_name, assign_name='', constraints=False, deform_bone='SAME'):
     """ Makes a copy of the given bone in the given armature object.
         Returns the resulting bone's name.
 
         NOTE: taken from rigify module utils.py, added the contraints option and stripped the part about rna properties
     """
-    if bone_name not in obj.data.edit_bones:
+    if bone_name not in ob.data.edit_bones:
         raise Exception("copy_bone(): bone '%s' not found, cannot copy it" % bone_name)
 
-    if obj == bpy.context.active_object and bpy.context.mode == 'EDIT_ARMATURE':
-        if assign_name == '':
-            assign_name = bone_name
-        # Copy the edit bone
-        edit_bone_1 = obj.data.edit_bones[bone_name]
-        edit_bone_2 = obj.data.edit_bones.new(assign_name)
-        bone_name_1 = bone_name
-        bone_name_2 = edit_bone_2.name
+    if assign_name == '':
+        assign_name = bone_name
+    # Copy the edit bone
+    edit_bone_1 = ob.data.edit_bones[bone_name]
+    edit_bone_2 = ob.data.edit_bones.new(assign_name)
 
-        edit_bone_2.parent = edit_bone_1.parent
-        edit_bone_2.use_connect = edit_bone_1.use_connect
+    bone_name_1 = bone_name
+    bone_name_2 = edit_bone_2.name
 
-        # Copy edit bone attributes
-        edit_bone_2.layers = list(edit_bone_1.layers)
+    edit_bone_2.parent = edit_bone_1.parent
+    edit_bone_2.use_connect = edit_bone_1.use_connect
 
-        edit_bone_2.head = Vector(edit_bone_1.head)
-        edit_bone_2.tail = Vector(edit_bone_1.tail)
-        edit_bone_2.roll = edit_bone_1.roll
+    # Copy edit bone attributes
+    edit_bone_2.layers = list(edit_bone_1.layers)
 
-        edit_bone_2.use_inherit_rotation = edit_bone_1.use_inherit_rotation
-        edit_bone_2.use_inherit_scale = edit_bone_1.use_inherit_scale
-        edit_bone_2.use_local_location = edit_bone_1.use_local_location
+    edit_bone_2.head = Vector(edit_bone_1.head)
+    edit_bone_2.tail = Vector(edit_bone_1.tail)
+    edit_bone_2.roll = edit_bone_1.roll
 
+    edit_bone_2.use_inherit_rotation = edit_bone_1.use_inherit_rotation
+    edit_bone_2.use_inherit_scale = edit_bone_1.use_inherit_scale
+    edit_bone_2.use_local_location = edit_bone_1.use_local_location
+
+    if deform_bone == 'SAME':
         edit_bone_2.use_deform = edit_bone_1.use_deform
-        edit_bone_2.bbone_segments = edit_bone_1.bbone_segments
-        edit_bone_2.bbone_custom_handle_start = edit_bone_1.bbone_custom_handle_start
-        edit_bone_2.bbone_custom_handle_end = edit_bone_1.bbone_custom_handle_end
-
-        bpy.ops.object.mode_set(mode='OBJECT')
-
-        # Get the pose bones
-        pose_bone_1 = obj.pose.bones[bone_name_1]
-        pose_bone_2 = obj.pose.bones[bone_name_2]
-
-        # Copy pose bone attributes
-        pose_bone_2.rotation_mode = pose_bone_1.rotation_mode
-        pose_bone_2.rotation_axis_angle = tuple(pose_bone_1.rotation_axis_angle)
-        pose_bone_2.rotation_euler = tuple(pose_bone_1.rotation_euler)
-        pose_bone_2.rotation_quaternion = tuple(pose_bone_1.rotation_quaternion)
-
-        pose_bone_2.lock_location = tuple(pose_bone_1.lock_location)
-        pose_bone_2.lock_scale = tuple(pose_bone_1.lock_scale)
-        pose_bone_2.lock_rotation = tuple(pose_bone_1.lock_rotation)
-        pose_bone_2.lock_rotation_w = pose_bone_1.lock_rotation_w
-        pose_bone_2.lock_rotations_4d = pose_bone_1.lock_rotations_4d
-
-        if constraints:
-            copy_bone_constraints(pose_bone_1, pose_bone_2)
-
-        return bone_name_2
     else:
-        raise Exception("Cannot copy bones outside of edit mode")
+        edit_bone_2.use_deform = deform_bone
+    edit_bone_2.bbone_segments = edit_bone_1.bbone_segments
+    edit_bone_2.bbone_custom_handle_start = edit_bone_1.bbone_custom_handle_start
+    edit_bone_2.bbone_custom_handle_end = edit_bone_1.bbone_custom_handle_end
+
+    # ITD- bones go to MCH layer
+    edit_bone_2.layers[30] = True
+    edit_bone_2.layers[31] = False
+    for i in range(30):
+        edit_bone_2.layers[i] = False
+
+    ob.update_from_editmode()
+
+    # Get the pose bones
+    pose_bone_1 = ob.pose.bones[bone_name_1]
+    pose_bone_2 = ob.pose.bones[bone_name_2]
+
+    # Copy pose bone attributes
+    pose_bone_2.rotation_mode = pose_bone_1.rotation_mode
+    pose_bone_2.rotation_axis_angle = tuple(pose_bone_1.rotation_axis_angle)
+    pose_bone_2.rotation_euler = tuple(pose_bone_1.rotation_euler)
+    pose_bone_2.rotation_quaternion = tuple(pose_bone_1.rotation_quaternion)
+
+    pose_bone_2.lock_location = tuple(pose_bone_1.lock_location)
+    pose_bone_2.lock_scale = tuple(pose_bone_1.lock_scale)
+    pose_bone_2.lock_rotation = tuple(pose_bone_1.lock_rotation)
+    pose_bone_2.lock_rotation_w = pose_bone_1.lock_rotation_w
+    pose_bone_2.lock_rotations_4d = pose_bone_1.lock_rotations_4d
+
+    if constraints:
+        copy_bone_constraints(pose_bone_1, pose_bone_2)
+
+    return bone_name_2
 
 
 def remove_bone_constraints(pbone):
@@ -140,7 +148,181 @@ def find_def_parent(ob, org_bone):
         return find_def_parent(ob, org_par)
 
 
-def gamefriendly_hierarchy(ob):
+def get_deform_root_name(ob):
+    """Get the name first deform bone with no deform parent
+
+    :param ob:
+    :return:
+    """
+    # TODO
+    return 'DEF-spine'
+
+
+def get_deform_hips_name(ob, bone_name=None):
+    """Starting from the root, get the first bone with more than one child
+
+    :param ob: the armature object
+    :param bone_name:
+    :return: name of deform hips bone
+    """
+    if not bone_name:
+        bone_name = get_deform_root_name(ob)
+
+    bone = ob.data.edit_bones[bone_name]
+
+    if len(bone.children) > 1:
+        return bone_name
+
+    return get_deform_hips_name(ob, bone.children[0].name)
+
+
+def set_inherit_scale(ob, inherit_mode='FIX_SHEAR'):
+    for bone in ob.data.edit_bones:
+        if not bone.use_deform:
+            continue
+
+        bone.inherit_scale = inherit_mode
+
+
+def copy_chain(ob, first, last_excluded=None, flip_bones=False):
+    bone = first
+
+    prev_itd_bone = None
+    while bone != last_excluded:
+        bone_name = bone.name
+        itd_name = bone_name.replace("DEF-", "ITD-")
+        try:
+            itd_bone = ob.data.edit_bones[itd_name]
+        except KeyError:
+            itd_name = copy_bone(ob, bone_name, assign_name=itd_name, constraints=True, deform_bone=False)
+            itd_bone = ob.data.edit_bones[itd_name]
+
+        itd_bone.use_deform = False
+        itd_bone.parent = prev_itd_bone
+        prev_itd_bone = itd_bone
+
+        cp_name = copy_bone(ob, bone_name, assign_name=bone_name.replace("DEF-", "CP-"), constraints=False, deform_bone=False)
+        cp_bone = ob.data.edit_bones[cp_name]
+        cp_bone.use_deform = False
+
+        cp_bone.parent = None
+
+        if flip_bones:
+            flip_bone(cp_bone)
+
+        pbone = ob.pose.bones[bone_name]
+        remove_bone_constraints(pbone)
+        cp_loc = pbone.constraints.new('COPY_LOCATION')
+        cp_rot = pbone.constraints.new('COPY_ROTATION')
+        cp_scale = pbone.constraints.new('COPY_SCALE')
+        for constr in (cp_loc, cp_rot, cp_scale):
+            constr.target = ob
+            constr.subtarget = cp_name
+
+        cp_bone.parent = itd_bone
+
+        # ITD- bones go to MCH layer
+        for new_bone in (itd_bone, cp_bone):
+            new_bone.layers[30] = True
+            new_bone.layers[31] = False
+            for i in range(30):
+                new_bone.layers[i] = False
+
+        if not bone.children:
+            break
+        bone = bone.children[0]
+
+
+def flip_bone(bone):
+    bone.head, bone.tail = bone.tail.copy(), bone.head.copy()
+
+
+def fix_tail_direction(ob):
+    """Make the hips the actual root and parent the tail to it (Rigify tails are the other way around"""
+    def_root_name = get_deform_root_name(ob)
+    def_hips_name = get_deform_hips_name(ob, def_root_name)
+
+    if def_root_name == def_hips_name:
+        return
+
+    def_root_edit = get_edit_bone(ob, def_root_name)
+    def_hips_edit = get_edit_bone(ob, def_hips_name)
+
+    copy_chain(ob, def_root_edit, def_hips_edit, flip_bones=True)
+
+    def_tail_next = def_hips_edit.parent
+    def_tail_previous = def_hips_edit
+    
+    def_hips_edit.parent = None
+    def_root_edit.parent = def_hips_edit
+
+    while def_tail_next:
+        if def_tail_next == def_hips_edit:
+            break
+        previous_parent = def_tail_next.parent
+        
+        def_tail_next.parent = None
+        # flip bone
+        flip_bone(def_tail_next)
+        def_tail_next.parent = def_tail_previous
+        if def_tail_previous is not def_hips_edit:
+            def_tail_next.use_connect = True
+
+        def_tail_previous = def_tail_next
+        def_tail_next = previous_parent
+
+    return def_hips_name
+
+
+def copytransform_to_copylocrot(ob):
+    for bone in ob.pose.bones:
+        if not ob.data.bones[bone.name].use_deform:
+            continue
+
+        to_remove = []
+        for constr in bone.constraints:
+            if constr.type == 'COPY_TRANSFORM':
+                to_remove.append(constr)
+                for cp_constr in (pbone.constraints.new('COPY_ROTATION'), pbone.constraints.new('COPY_LOCATION')):
+                    cp_constr.target = constr.ob
+                    cp_constr.subtarget = constr.subtarget
+            elif constr.type == 'STRETCH_TO':
+                constr.mute = True
+
+        for constr in to_remove:
+            bone.constraints.remove(constr)
+
+
+def limit_spine_scale(ob):
+    for pbone in ob.pose.bones:
+        if not ob.data.bones[pbone.name].use_deform:
+            continue
+
+        if not pbone.name.startswith('DEF-spine'):
+            continue
+
+        constr = pbone.constraints.new('LIMIT_SCALE')
+
+        constr.min_x = 1
+        constr.min_y = 1
+        constr.min_z = 1
+
+        constr.max_x = 1
+        constr.max_y = 1
+        constr.max_z = 1
+
+        constr.use_min_x = True
+        constr.use_min_y = True
+        constr.use_min_z = True
+
+        constr.use_max_x = True
+        constr.use_max_y = True
+        constr.use_max_z = True
+
+        constr.owner_space = 'LOCAL'
+
+
+def gamefriendly_hierarchy(ob, fix_tail=True, limit_scale=False):
     """Changes Rigify (0.5) rigs to a single root deformation hierarchy.
     Create ITD- (InTermeDiate) bones in the process"""
     assert(ob.mode == 'EDIT')
@@ -148,11 +330,15 @@ def gamefriendly_hierarchy(ob):
     pbones = list(ob.pose.bones)  # a list of pose bones is safer when switching through modes
     new_bone_names = []  # collect newly added bone names so that they can be edited later in Object Mode
 
+    def_root_name = get_deform_root_name(ob)
+
     # we want deforming bone (i.e. the ones on layer 29) to have deforming bone parents
     for pbone in pbones:
         bone_name = pbone.name
 
         if not is_def_bone(ob, bone_name):
+            continue
+        if bone_name == def_root_name:
             continue
         if not pbone.parent:
             # root bones are fine
@@ -165,25 +351,20 @@ def gamefriendly_hierarchy(ob):
             continue
 
         # Intermediate Bone
-        itd_name = copy_bone(ob, bone_name, assign_name=bone_name.replace("DEF-", "ITD-"), constraints=True)
-        new_bone_names.append(itd_name)
-
-        itd_bone = ob.data.bones[itd_name]
-        itd_bone.use_deform = False
-
-        # ITD- bones go to MCH layer
-        itd_bone.layers[30] = True
-        itd_bone.layers[31] = False
-        for i in range(30):
-            itd_bone.layers[i] = False
-
-        bpy.ops.object.mode_set(mode='EDIT')  # bone copy has set Object Mode
+        itd_name = bone_name.replace("DEF-", "ITD-")
+        try:
+            ob.data.edit_bones[itd_name]
+        except KeyError:
+            itd_name = copy_bone(ob, bone_name, assign_name=bone_name.replace("DEF-", "ITD-"), constraints=True, deform_bone=False)
+            new_bone_names.append(itd_name)
 
         # DEF- bone will now follow the ITD- bone
         remove_bone_constraints(pbone)
-        cp_trans = pbone.constraints.new('COPY_TRANSFORMS')
-        cp_trans.target = ob
-        cp_trans.subtarget = itd_name
+        for cp_constr in (pbone.constraints.new('COPY_LOCATION'),
+                          pbone.constraints.new('COPY_ROTATION'),
+                          pbone.constraints.new('COPY_SCALE')):
+            cp_constr.target = ob
+            cp_constr.subtarget = itd_name
 
         # Look for a DEF- bone that would be a good parent. Unlike DEF- bones, ORG- bones retain the
         # hierarchy from the metarig, so we are going to reproduce the ORG- hierarchy
@@ -220,13 +401,37 @@ def gamefriendly_hierarchy(ob):
         ebone_par = get_edit_bone(ob, def_par.name)
         ebone.parent = ebone_par
 
+    if fix_tail:
+        def_root_name = fix_tail_direction(ob)
+
+    if limit_scale:
+        limit_spine_scale(ob)
+
+    ob.data.edit_bones[def_root_name].parent = ob.data.edit_bones['root']
+
 
 class ConvertGameFriendly(bpy.types.Operator):
     """Convert Rigify (0.5) rigs to a Game Friendly hierarchy"""
     bl_idname = "armature.rigify_convert_gamefriendly"
-    bl_label = "Make the rigify deformation bones a one root rig"
+    bl_label = "Rigify Game Friendly"
+    bl_description = "Make the rigify deformation bones a one root rig"
+    bl_options = {'REGISTER', 'UNDO'}
 
-    keep_backup = bpy.props.BoolProperty("Keep copy of datablock", default=True)
+    keep_backup: BoolProperty(
+        name="Backup",
+        description="Keep copy of datablock",
+        default=True
+    )
+    limit_scale: BoolProperty(
+        name="Limit Spine Scale",
+        description="Limit scale on the spine deform bones",
+        default=True
+    )
+    fix_tail: BoolProperty(
+        name="Invert Tail",
+        description="Reverse the tail direction so that it spawns from hip",
+        default=True
+    )
 
     @classmethod
     def poll(cls, context):
@@ -246,7 +451,7 @@ class ConvertGameFriendly(bpy.types.Operator):
             backup_data.name = ob.name + "_GameUnfriendly_backup"
             backup_data.use_fake_user = True
 
-        gamefriendly_hierarchy(ob)
+        gamefriendly_hierarchy(ob, fix_tail=self.fix_tail, limit_scale=self.limit_scale)
         return {'FINISHED'}
 
 
@@ -267,18 +472,27 @@ class DATA_PT_rigify_makefriendly(bpy.types.Panel):
         if obj.mode == 'EDIT':
             pass
 
+        # TODO: remove button, use right clic in edit mode
         row = layout.row()
         props = layout.operator("armature.rigify_convert_gamefriendly", text="Make Game Friendly")
+
+
+def menu_func(self, context):
+    self.layout.operator("armature.rigify_convert_gamefriendly")
 
 
 def register():
     bpy.utils.register_class(ConvertGameFriendly)
     bpy.utils.register_class(DATA_PT_rigify_makefriendly)
 
+    bpy.types.VIEW3D_MT_armature_context_menu.append(menu_func)
+
 
 def unregister():
     bpy.utils.unregister_class(ConvertGameFriendly)
     bpy.utils.unregister_class(DATA_PT_rigify_makefriendly)
+
+    bpy.types.VIEW3D_MT_armature_context_menu.append(menu_func)
 
 
 if __name__ == "__main__":
